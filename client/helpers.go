52a0f7429600d68dcb53186fd1b3ee54d5c6950f
Luca Daniel <lucadanielcostin@gmail.com>
2018-04-30 15:16:15 +0300

    Update helpers.go

--- client/helpers.go
+++ client/helpers.go
@@ -5,12 +5,12 @@ import (
 	"strings"
 )
 
-func trim0x(PrefixedString string) string {
-	return strings.TrimPrefix(PrefixedString, "0x")
+func trim0x(prefixedString string) string {
+	return strings.TrimPrefix(prefixedString, "0x")
 }
 
-func hexStrToBigInt(HexString string) *big.Int {
+func hexStrToBigInt(hexString string) *big.Int {
 	value := new(big.Int)
-	value.SetString(trim0x(HexString), 16)
+	value.SetString(trim0x(hexString), 16)
 	return value
 }
